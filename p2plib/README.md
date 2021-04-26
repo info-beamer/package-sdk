b5f130cd7911e0b8e94dc892aae7b16645dda14a
Florian Wesch <fw@dividuum.de>
2021-04-26 19:48:18 +0200

    Added p2plib.py for Peer-to-Peer powered services

---
+++ p2plib/README.md
@@ -0,0 +1,263 @@
+# Peer-to-Peer (P2P) support library
+
+This library allows easy collaboration of [package services](https://info-beamer.com/doc/package-services)
+running on multiple co-located devices. It uses the [Peer-to-Peer feature](https://info-beamer.com/doc/device-configuration#p2p)
+for device detection, automatic leader selection and secure communication setup. 
+
+It requires info-beamer OS with a version later than '202104xx'.
+
+## Introduction
+
+The library can be used to synchronize content playback or other state across multiple devices running the
+same setup. Examples might be:
+
+ * The selected leader device decides which item within a playlist to play next and notifies all follower devices.
+ * An event (for example GPIO) triggered on a single device should trigger an action on all other devices as well.
+ * Some data (like a counter) value should be synced across multiple devices.
+
+A single device within the group of devices running the same setup is automatically promoted to leader. All
+other devices become followers. The leader can broadcast short (recommended size ~1KB) messages to all
+follower devices. Similarly any device can send a message to the current leader devices. The library can
+also be used if only a single device is active: All messages are then locally delivery within the same program.
+This allows you to use the same code regardless of the number of active devices.
+
+The library automatically handles device detection within the local network using features provided by the
+info-beamer OS. It then uses knowledge of other devices to select a single 'leader' among those devices.
+It automatically sets up cryptographic keys that allow secure two-way communication across the devices.
+
+For communication it uses UDP. The protocol guarantees at-most-once delivery of messages to other devices.
+Due to the lossy nature of UDP it cannot guarantee delivery though. So you should be prepared to lose
+messages if the network isn't reliably. Using Ethernet is preferred to WiFi. Message size should not exceed
+around 1KB when serialized as JSON. The protocol overhead is around 40 bytes per message.
+
+Clocks on all participating devices must be reasonably in sync (at most 3 seconds difference) for
+communication to work. This also means that it won't work on completely offline devices that don't
+have a correct time at all.
+
+A network split might result in multiple leaders. Usually detection of new or disappeared peers happens
+within 5-10 seconds and results in a newly selected leader.
+
+## Example Code
+
+```python
+#!/usr/bin/python
+import time, sys, threading
+from p2plib import PeerGroup
+
+class ExampleGroup(PeerGroup):
+    def leader_thread(self):
+        while self._leader_running:
+            self.broadcast_to_all(
+                value = time.time(),
+            )
+            time.sleep(1)
+
+    def promote_leader(self, peer_info):
+        print >>sys.stderr, "Now a leader"
+        self._leader_running = True
+        self._leader_thread = threading.Thread(target=self.leader_thread)
+        self._leader_thread.daemon = True
+        self._leader_thread.start()
+
+    def demote_leader(self):
+        print >>sys.stderr, "No longer a leader"
+        self._leader_running = False
+        self._leader_thread.join()
+
+    def on_leader_message(self, message, peer_info):
+        print >>sys.stderr, "received message from leader %r: %r" % (peer_info, message)
+
+if __name__ == "__main__":
+    group = ExampleGroup()
+    while 1:
+        time.sleep(1)
+```
+
+The ExampleGroup is based on the PeerGroup class. When instantiated it
+automatically starts two threads handling peer detection and network
+communication. After a short moment the `promote_leader` callback
+will be called on a single device. In the example code this then starts
+its own worker thread that sends a short message every second to all
+other peers (including the leader peer itself) using `broadcast_to_all`.
+
+On all devices running the same setup, the `on_leader_message` will be
+called for each message sent by the leader. In the example the message
+is written to stderr (and ends up in the log).
+
+Should the current leader device go offline, it might take a few
+seconds for all remaining devices to select a new leader.
+The `promote_leader` will again be called on the selected device.
+
+Should the previous offline leader return, the current leader will
+be demoted and its `demote_leader` callback will be called. In the
+example this shut down the thread.
+
+Similarly a device can also send messages to the current leader
+using `send_to_leader`. On the leader, such messages will
+be delivered to the `on_peer_message` callback.
+
+## Hints
+
+If a device has disabled support Peer-to-Peer support or a device
+is alone in its current networking environment, a PeerGroup
+will consist of only this one device and it will be selected
+as the leader. `broadcast_to_all` and `send_to_leader` will be
+local method calls in that case. This allows you to use the
+same code regardless of the Peer-to-Peer state on a device as
+it transparently scales back from multiple devices to just a
+single one.
+
+A device should always handle lost messages. If the state you need
+to sync is reasonably small, it might be easiest to just sync the
+state from leader to follower every second. A lost message will
+be hopefully received on a later attempt.
+
+For precise timing it's recommended to send an exact future
+timestamp within the message itself. That way all devices will
+eventually receive the message and can begin acting on it based
+on the timestamp within the message itself. The protocol itself
+makes no guarantee with regards to latency. So you should not use
+the `broadcast_to_all` to directly trigger events within the
+`on_leader_message` callback.
+
+# API
+
+The following API calls are provided by the PeerGroup class.
+The API is threadsafe and any property or method can be used
+within all callbacks or by an external caller.
+
+## Constructor `PeerGroup(port=None)`
+
+Sets up a new peer group. The optional port argument can be provided
+for force the use of a specific UDP port for communication. Usually
+this is not recommended as an automatic port is selected based
+on the current node directory.
+
+Currently there is no way to shut down a constructed PeerGroup.
+The code you write should always handle an unclean device shutdown
+or termination of the process.
+
+## Callback `group.setup_peer(self)`
+
+This call is invoked once after the PeerGroup is fully set up.
+It can be used to handle one-time initializations required on
+all devices.
+
+## Callback `group.promote_leader(self, peer_info)`
+
+Called on the currently selected leader. Set up required data structures
+needed to fulfil the leader role. This can include starting new background
+threads. `peer_info` provides information about the leader.
+
+## Callback `group.demote_leader(self)`
+
+Called if the device is no longer the leader. You should shut down
+any resources allocated in the `promote_leader` callback.
+
+## Callback `group.promote_follower(self, peer_info)`
+
+Called on a device if it's not a leader. Usually not required as
+the distinction between a follower (so any device not being the leader)
+and all devices (so leader and all followers) might not be useful.
+Usually all devices (including the leader device itself) handle
+data send by the leader and you don't need the follower distinction.
+`peer_info` provides information about the follower.
+
+## Callback `group.demote_follower(self)`
+
+Called on a devices if it's no longer a follower.
+
+## Callback `group.on_peer_added(self, peer_info)`
+
+Called if a new peer is added to the group. This callback is called
+before a peer with the same `ip` value in `peer_info` is provided in
+an eventual call to `promote_follower` or `promote_leader`.
+
+## Callback `group.on_peer_removed(self, peer_info)`
+
+Called if a peer is no longer available. This callback is called
+after a peer is demoted.
+
+## Callback `group.on_leader_message(msg, peer_info)`
+
+Called on a device if a message from the current leader is received.
+`msg` is the data sent by `broadcast_to_all`. `peer_info` is some
+metadata about the current leader. See below.
+
+## Callback `group.on_peer_message(msg, peer_info)`
+
+Called on the leader device if any of the other peers calls
+`send_to_leader`. `msg` is the sent data and `peer_info` provides
+information about the current leader.
+
+## Property `group.is_follower`
+
+True if the device is a follower (so not a leader).
+
+## Property `group.is_follower`
+
+True if the device is the selected leader.
+
+## Property `group.num_peers`
+
+Returns the total number of devices (including the one calling)
+within the PeerGroup.
+
+## Property `group.peers`
+
+Returns a list of `peer_info` objects for all peers.
+
+## Method `group.broadcast_to_all(**message)`
+
+This method can be called on the leader to send the message
+to all devices within the PeerGroup. If the device isn't
+the leader, the call has no effect.
+
+The data in `message` must be serializable as JSON and the
+resulting serialized version should not exceed around 1KB
+in total size. Otherwise message delivery might not be as
+reliable as the resulting UDP paket will be fragmented.
+It's best to only send short messages.
+
+Note that this method also invokes `on_leader_message`
+on the same local PeerGroup instance itself using a local
+method call.
+
+Message delivery is not guaranteed (as UDP is used for
+transport and devices might disappear or be unreachable
+at any time). The transport protocol guarantees at-most-once
+delivery though if the message rate does not exceed
+300 messages/second.
+
+## Method `group.send_to_leader(self, **message)`
+
+This method send a message from any device to the current
+leader. It can be called on any device within the
+current PeerGroup. If it's called on the leader itself,
+a local method call to `on_peer_message` will be issued
+instead of using network communication.
+
+The data in `message` must be serializable as JSON and the
+resulting serialized version should not exceed around 1KB
+in total size. Otherwise message delivery might not be as
+reliable as the resulting UDP paket will be fragmented.
+It's best to only send short messages.
+
+Message delivery is not guaranteed (as UDP is used for
+transport and devices might disappear or be unreachable
+at any time). The transport protocol guarantees at-most-once
+delivery though if the message rate does not exceed
+300 messages/second.
+
+## `peer_info` Object
+
+The `peer_info` object provides basic information about known peers.
+It defines three properties: 
+
+ * `ip` is the IP address of the peer. This is always '127.0.0.1' for this device.
+ * `device_id` is the device id of the device.
+ * `delta` is a timing delta and provides an estimated time offset to the local device in seconds.
+
+If you need to keep a mapping from `peer_info` to your own data, use the
+`ip` value as key, as it's the only value guaranteed to be unique across
+different peers.
---
+++ p2plib/p2plib.py
@@ -0,0 +1,457 @@
+#
+# Part of info-beamer hosted. You can find the latest version
+# of this file at:
+#
+# https://github.com/info-beamer/package-sdk
+#
+# Copyright (c) 2021 Florian Wesch <fw@info-beamer.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#     Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the
+#     distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+VERSION = "1.0"
+
+import requests, threading, time, sys, traceback, socket, json, hmac, hashlib, struct, os
+from Crypto import Random
+from Crypto.Cipher import AES
+from binascii import unhexlify, hexlify
+from collections import namedtuple
+
+# This service uses its own UDP protocol. Multiple nodes might use
+# this service. To avoid port collisions, based on a base port, a
+# unique port number is derived for each node within the assigned
+# setup.
+P2P_GROUP_BASE_PORT = 61234
+
+# The avoid delivering duplicate messages (either accidental or
+# maliciously), each peer keeps a list of recently received messages.
+# To avoid keeping too many of those the following limits the
+# saved message ids.  
+MAX_MESSAGE_PER_SEC = 300
+
+def log(msg, name='p2plib.py'):
+    print >>sys.stderr, "[{}] {}".format(name, msg)
+
+PeerInfo = namedtuple('PeerInfo', 'ip device_id delta')
+
+class Peer(object):
+    def __init__(self, ip):
+        self._ip = ip
+
+        # protocol version
+        self._version = 1
+
+        # throw away packets if the device's time is off by more than x seconds
+        self._discard_time_diff = 3
+
+        # state to handle throwing away duplicate messages. The delivery
+        # guarantees at-most-once semantics.
+        self._last_cleanup = 0
+        self._max_msg_ids = MAX_MESSAGE_PER_SEC * self._discard_time_diff
+        self._msg_id_order = []
+        self._msg_id_set = set()
+
+    def update(self, device_id, pair_key, delta, is_leader):
+        self._device_id = device_id
+        self._pair_key = pair_key
+        self._delta = delta
+        self._is_leader = is_leader
+
+    def add_seen_msg_id(self, msg_id, now):
+        if msg_id in self._msg_id_set:
+            return False
+        if now > self._last_cleanup + 1 or len(self._msg_id_order) > self._max_msg_ids:
+            self.cleanup_msg_ids(now)
+            if len(self._msg_id_order) > self._max_msg_ids:
+                return False
+        discard_threshold = now + self._discard_time_diff
+        if self._msg_id_order and discard_threshold < self._msg_id_order[-1][0]:
+            # Our clock went backwards
+            return False
+        # self._msg_id_order is sorted by discard_threshold
+        self._msg_id_order.append((discard_threshold, msg_id))
+        self._msg_id_set.add(msg_id)
+        return True
+
+    def cleanup_msg_ids(self, now):
+        deleted = 0
+        while self._msg_id_order:
+            discard_threshold, msg_id = self._msg_id_order[0]
+            if now < discard_threshold:
+                break
+            self._msg_id_order.pop(0)
+            self._msg_id_set.remove(msg_id)
+            deleted += 1
+        # log('cleaned up %d msg ids' % (deleted,))
+        self._last_cleanup = now
+
+    @property
+    def is_leader(self):
+        return self._is_leader
+
+    @property
+    def device_id(self):
+        return self._device_id
+
+    @property
+    def ip(self):
+        return self._ip
+
+    @property
+    def peer_info(self):
+        return PeerInfo(self._ip, self._device_id, self._delta)
+
+    def encode(self, data, direction):
+        #
+        # |     16      |     16      0   1           5                           x (%16=0)
+        # |             |             | I | timestamp | {json message} ' <pad>  ' |
+        # |             |             |                                           |
+        # |             |- msg_id/IV -+-------------- Message --------------------|
+        # |             |     '---------------------.----'                        |
+        # |             |                        AES CBC                          |
+        # |             |                           |                             |
+        # |             |                           v                             |
+        # |             |---------------------- Ciphertext -----------------------'
+        # |      .---------- HMAC ------------------'                             |
+        # |      v      |                                                         |
+        # |---- MAC ----|                                                         |
+        # 
+        message = struct.pack("<BL", 
+            direction << 7 | self._version, # info_bytes
+            time.time()
+        )
+        message += json.dumps(
+            data,
+            ensure_ascii=False,
+            separators=(',',':'),
+        ).encode('utf8')
+        message += ' ' * (16 - (len(message)-1) % 16 - 1) # pad
+        msg_id = Random.get_random_bytes(16)
+        cipher = AES.new(self._pair_key, AES.MODE_CBC, msg_id)
+        encrypted = msg_id + cipher.encrypt(message)
+        return hmac.HMAC(
+            self._pair_key,
+            encrypted,
+            hashlib.sha256
+        ).digest()[:16] + encrypted
+
+    def decode(self, pkt, expected_direction):
+        now = time.time()
+        if now < 1000000000:
+            # Don't receive with wrong local time
+            return None
+        mac, encrypted = pkt[:16], pkt[16:]
+        if len(mac) != 16:
+            return None
+        if hmac.HMAC(
+            self._pair_key,
+            encrypted,
+            hashlib.sha256
+        ).digest()[:16] != mac:
+            return None
+        msg_id, ciphertext = encrypted[:16], encrypted[16:]
+        if len(msg_id) != 16:
+            return None
+        if len(ciphertext) % 16:
+            return None
+        cipher = AES.new(self._pair_key, AES.MODE_CBC, msg_id)
+        message = cipher.decrypt(ciphertext)
+        hdr, data = message[:5], message[5:]
+        info_byte, remote_ts = struct.unpack("<BL", hdr)
+        version = info_byte & 0x7f
+        if version != self._version:
+            return None
+        direction = info_byte >> 7
+        if direction != expected_direction:
+            return None
+        # remote and our time too far off?
+        if now >= remote_ts + self._discard_time_diff:
+            return None
+        if remote_ts <= now - self._discard_time_diff:
+            return None
+        if not self.add_seen_msg_id(msg_id, now):
+            return None
+        try:
+            return json.loads(data)
+        except Exception as err:
+            return None
+
+    def __repr__(self):
+        return '<%d: %s>' % (self._device_id, self._ip)
+
+ROLE_LEADER, ROLE_FOLLOWER = 1, 2
+DIRECTION_LEADER_TO_PEER, DIRECTION_PEER_TO_LEADER = 0, 1
+
+class PeerGroup(object):
+    def __init__(self, port=None):
+        # This assumed that the service is running within its node
+        # directory.
+        with open('config.json') as f:
+            metadata = json.load(f)['__metadata']
+
+        if port is not None:
+            self._port = port
+        else:
+            self._port = P2P_GROUP_BASE_PORT + metadata.get('node_idx', 0)
+
+        # Every node on the device can calculate its own
+        # unicode node_scope value based on port, instance_id
+        # and current work directory. Multiple devices
+        # with the same setup assigned will all calculate
+        # the same value. This avoid reusing the same
+        # pairwise keys across multiple peer groups running
+        # in different package services.
+        self._node_scope = hashlib.sha256(
+            '%d:%d:%s' % (
+                self._port, metadata['instance_id'], os.getcwd(),
+            )
+        ).digest()
+
+        self._no_p2p_fallback = [dict(
+            device_id = metadata['device_id'],
+            pair_key = '0' * 16,
+            delta = 0,
+            ip = '127.0.0.1',
+        )]
+
+        log("using peer group port %d, node scope %s" % (
+            self._port, hexlify(self._node_scope)
+        ))
+
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._sock.bind(('0.0.0.0', self._port))
+
+        self._role = None
+        self._me = None
+        self._leader = None
+        self._peers, self._peers_lock = {}, threading.Lock()
+
+        thread = threading.Thread(target=self._update_thread)
+        thread.daemon = True
+        thread.start()
+
+        thread = threading.Thread(target=self._listen_thread)
+        thread.daemon = True
+        thread.start()
+
+        self.setup_peer()
+
+    ##----- Methods to overwrite ------
+
+    def setup_peer(self):
+        pass
+
+    def promote_leader(self, peer_info):
+        pass
+
+    def demote_leader(self):
+        pass
+
+    def promote_follower(self, peer_info):
+        pass
+
+    def demote_follower(self):
+        pass
+
+    def on_peer_added(self, peer_info):
+        pass
+
+    def on_peer_removed(self, peer_info):
+        pass
+
+    # Handle message sent by broadcast_to_all.
+    # The message is received at most once on all peers.
+    def on_leader_message(self, msg, peer_info):
+        pass
+
+    # Handle message sent by send_to_leader.
+    # This callback is only called when this peer is
+    # a leader.
+    def on_peer_message(self, msg, peer_info):
+        pass
+
+    ##----- Public Methods/Properties  -----
+
+    @property
+    def is_follower(self):
+        return self._role == ROLE_FOLLOWER
+
+    @property
+    def is_leader(self):
+        return self._role == ROLE_LEADER
+
+    @property
+    def num_peers(self):
+        with self._peers_lock:
+            return len(self._peers)
+
+    @property
+    def peers(self):
+        peers = []
+        with self._peers_lock:
+            for ip, peer in self._peers.iteritems():
+                peers.append(peer.peer_info)
+        peers.sort()
+        return peers
+
+    def broadcast_to_all(self, **message):
+        if self._role != ROLE_LEADER:
+            return
+        local_device = None
+        with self._peers_lock:
+            for ip, peer in self._peers.iteritems():
+                if peer is self._me:
+                    local_device = self._me
+                else:
+                    pkt = peer.encode(message,
+                        direction = DIRECTION_LEADER_TO_PEER
+                    )
+                    self._sock.sendto(pkt, (ip, self._port))
+        if local_device is not None:
+            self.on_leader_message(json.loads(json.dumps(message)), local_device.peer_info)
+
+    def send_to_leader(self, **message):
+        local_device = None
+        with self._peers_lock:
+            if self._leader is self._me:
+                local_device = self._me
+            else:
+                pkt = self._leader.encode(message,
+                    direction = DIRECTION_PEER_TO_LEADER
+                )
+                self._sock.sendto(pkt, (self._leader.ip, self._port))
+        if local_device is not None:
+            self.on_peer_message(json.loads(json.dumps(message)), local_device.peer_info)
+
+    ##--------------------------------
+
+    def _listen_thread(self):
+        while 1:
+            try:
+                pkt, (ip, port) = self._sock.recvfrom(2**16)
+                if port != self._port:
+                    continue
+                receiver = None
+                with self._peers_lock:
+                    peer = self._peers.get(ip)
+                    if peer is None:
+                        continue
+                    if peer.is_leader and self._role == ROLE_FOLLOWER:
+                        receiver = self.on_leader_message
+                        message = peer.decode(pkt,
+                            expected_direction = DIRECTION_LEADER_TO_PEER
+                        )
+                    elif not peer.is_leader and self._role == ROLE_LEADER:
+                        receiver = self.on_peer_message
+                        message = peer.decode(pkt,
+                            expected_direction = DIRECTION_PEER_TO_LEADER
+                        )
+                    else:
+                        continue
+                receiver(message, peer.peer_info)
+            except Exception as err:
+                traceback.print_exc()
+
+    def _update_thread(self):
+        while 1:
+            try:
+                r = requests.get(
+                    'http://127.0.0.1:81/api/v1/peers/setup', timeout=0.5
+                )
+                peers = r.json()['peers']
+                if not peers:
+                    # If P2P is disabled or P2P traffic completely blocked
+                    # for some reason, use a fallback list to make this
+                    # device its own leader.
+                    peers = self._no_p2p_fallback
+                self._update_peers(peers)
+            except Exception as err:
+                log('cannot update setup peers: %s' % err)
+                traceback.print_exc()
+            time.sleep(3)
+
+    def _update_peers(self, peers):
+        me, leader = None, None
+        seen = set()
+        added, deleted = set(), set()
+        with self._peers_lock:
+            for idx, peer_info in enumerate(peers):
+                device_id = peer_info['device_id']
+                delta = peer_info['delta']
+                ip = peer_info['ip']
+                pair_key = hmac.HMAC(
+                    unhexlify(peer_info['pair_key']),
+                    self._node_scope,
+                    hashlib.sha256
+                ).digest()[:16] 
+                seen.add(ip)
+                peer = self._peers.get(ip)
+                is_added = peer is None
+                if is_added:
+                    log('added peer %s' % ip)
+                    peer = self._peers[ip] = Peer(ip)
+                peer.update(device_id, pair_key, delta, idx == 0)
+                if is_added:
+                    added.add(peer.peer_info)
+
+                # First device is always the leader. the peers/setup
+                # response is guaranteed to be sorted the same on all devices
+                # assuming they all know each other.
+                if idx == 0:
+                    leader = peer
+
+                # This device will always be marked by 127.0.0.1
+                if ip == '127.0.0.1':
+                    me = peer
+            known = set(self._peers.keys())
+            for ip in known - seen:
+                log('removed peer %s' % ip)
+                peer = self._peers.pop(ip)
+                deleted.add(peer.peer_info)
+
+        self._me = me
+        self._leader = leader 
+
+        if me is None:
+            new_role = None
+        else:
+            new_role = ROLE_LEADER if me == leader else ROLE_FOLLOWER
+
+        for peer_info in added:
+            self.on_peer_added(peer_info)
+
+        if new_role != self._role:
+            if self._role == ROLE_FOLLOWER:
+                self.demote_follower()
+            elif self._role == ROLE_LEADER:
+                self.demote_leader()
+            self._role = new_role
+            if self._role == ROLE_FOLLOWER:
+                self.promote_follower(me.peer_info)
+            elif self._role == ROLE_LEADER:
+                self.promote_leader(me.peer_info)
+
+        for peer_info in deleted:
+            self.on_peer_removed(peer_info)
